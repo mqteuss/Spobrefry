a961b09809d040aa08d544381f20829501d68c7d
mqteuss <mh.umateus@gmail.com>
2025-10-03 21:26:07 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'music-player-v5'; // Versão incrementada para forçar o recarregamento de TODAS as músicas.
+// Versão incrementada para forçar a atualização do cache
+const CACHE_NAME = 'music-player-v4'; 
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
@@ -8,12 +9,12 @@ const ASSETS_TO_CACHE = [
     '/images/default-cover.jpg',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
     
-    // --- MÚSICAS PARA CACHE OFFLINE (Total de 17) ---
+    // --- LISTA DE MÚSICAS PARA CACHE OFFLINE ---
     '/musics/Chris Grey - ALWAYS BEEN YOU.mp3',
     '/musics/DJ Snake - U Are My High (Feat. Future).mp3',
     '/musics/LEOWOLF - Romantic Gangster.mp3',
     '/musics/No. 1 Party Anthem.mp3',
-    '/musics/RealestK - Senfie.mp3',
+    '/musics/RealestK - Selfie.mp3',
     '/musics/The Weeknd.mp3',
     '/musics/Zayn - Lied To.mp3',
     '/musics/505-ArticMonkeys.mp3',
@@ -26,7 +27,6 @@ const ASSETS_TO_CACHE = [
     '/musics/GIVĒON - Heartbreak Anniversary (Audio)(MP3_128K).mp3',
     '/musics/GIVĒON - Make You Mine (Official Lyric Video)(MP3_128K).mp3',
     '/musics/Your Rarest of Flowers(MP3_128K).mp3'
-    // ------------------------------------------------
 ];
 
 self.addEventListener('install', event => {
@@ -37,35 +37,28 @@ self.addEventListener('install', event => {
                 return cache.addAll(ASSETS_TO_CACHE);
             })
     );
-    // Força o Service Worker a assumir o controle imediatamente
-    self.skipWaiting(); 
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil(
-        // Remove caches antigos para liberar espaço
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cache => {
                     if (cache !== CACHE_NAME) {
-                        console.log('Cache antigo removido:', cache);
+                        console.log('Cache antigo removido');
                         return caches.delete(cache);
                     }
                 })
             );
         })
     );
-    // Garante que o Service Worker atualize os clientes imediatamente
-    self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-    // Estratégia Cache-First: Tenta buscar no cache primeiro, se não encontrar, busca na rede.
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                // Se a requisição (música) for encontrada no cache, retorna a versão em cache.
-                // Caso contrário, tenta buscar na rede.
                 return response || fetch(event.request);
             })
     );
